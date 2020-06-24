f530e69c4a042ae5431197b17d7f234ee26fc28a
Erick Saravia <ericks@labs.aeegle.com>
2020-06-24 14:44:44 -0600

    update port

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import  { ExpressConfig }  from "../POO/express";
 
 const configExpress:ExpressConfig ={
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || 80,
     environment: process.env.ENVIRONMENT || "dev"
 }
 
