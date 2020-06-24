e24c0e31e0d022f61b7da47da0ff6b1a0ba270fa
Erick Saravia <ericks@labs.aeegle.com>
2020-06-24 17:58:14 -0600

    update try scraping

--- src/controllers/v1/simplex/src/scraper.ts
+++ src/controllers/v1/simplex/src/scraper.ts
@@ -8,36 +8,40 @@ async function getAnswer(page:any) {
 }
 
 async function scraper(url:string) {
-    const browser = await puppeteer.launch({
-        headless: true,
-        userDataDir: '/tmp',
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--js-flags=--expose-gc'],
-        defaultViewport: {
-            width: 1080,
-            height: 600
-        }
-    });
-
-    const page = await browser.newPage();
-
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36');
-
-    // just load DOM
-    await page.setRequestInterception(true);
-    page.on('request', (request:any) => {
-        if (request.resourceType() !== 'document') request.abort();
-        else request.continue();
-    });
-
-    console.log('----- load page')
-    await page.goto(url, { waitUntil: 'networkidle0', timeout: 0 })
-    const answerlink = '[href="solucion2.php?l=es"]'
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            userDataDir: '/tmp',
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--js-flags=--expose-gc'],
+            defaultViewport: {
+                width: 1080,
+                height: 600
+            }
+        });
     
-    await page.click(answerlink, true)
-    await page.waitFor(1000)
-    const answer = await getAnswer(page)
-    await browser.close()
-    return answer
+        const page = await browser.newPage();
+    
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36');
+    
+        // just load DOM
+        await page.setRequestInterception(true);
+        page.on('request', (request:any) => {
+            if (request.resourceType() !== 'document') request.abort();
+            else request.continue();
+        });
+    
+        console.log('----- load page')
+        await page.goto(url, { waitUntil: 'networkidle0', timeout: 0 })
+        const answerlink = '[href="solucion2.php?l=es"]'
+        
+        await page.click(answerlink, true)
+        await page.waitFor(1000)
+        const answer = await getAnswer(page)
+        await browser.close()
+        return answer
+    } catch (e) {
+        return "ERROR CON LA ENTRADA DE DATOS!!!"
+    }
 };
 
 export default scraper
