ca0c1e64b51b0f2371539c8e0791922f748f1344
Erick Saravia <ericks@labs.aeegle.com>
2020-06-24 16:58:15 -0600

    v1 finished

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import "dotenv/config"
 import {configExpress } from "./config/config"
 import { AppExpress } from "./POO/express/";
-
+import path from "path";
+import express from "express";
 import { Server } from "./controllers/index";
 
 const app = new AppExpress(configExpress, Server)
+app.addMiddleware(express.static(path.join(__dirname, 'public')))
 app.initializateControllers()
 app.listen()
